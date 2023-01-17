b072b799f12eaa93c41fba3adeaff62bdef201f0
zerefJS <onurakbas.342@gmail.com>
2023-01-17 16:52:43 +0300

    dropdown menu fixed

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const dropDownMenu = document.querySelectorAll(".sublist__container--items");
 const openMenu = document.querySelector(".open-menu");
 const closeMenu = document.querySelector(".close-menu");
 const listContainer = document.querySelector(".list__container");
-const main = document.querySelector("main")
+const main = document.querySelector("main");
+
 
 dropDown.forEach(item => {
     item.addEventListener("click", e => {
         e.stopPropagation();       
-        const [ target ] = e.target.children
-        console.log(target)
+        const [ image ] = e.target.children;
+        image.src = "./images/icon-arrow-up.svg"
+        const target = e.target.nextElementSibling
         target.classList.add("active");
     })
 }, false)
@@ -18,6 +20,8 @@ document.addEventListener("click", (e) => {
     if (!e.composedPath().includes(dropDown) && !e.composedPath().includes(dropDownMenu)) {
         dropDownMenu.forEach((el) => {
             el.classList.remove("active")
+            const [image] = el.previousElementSibling.children
+            image.src = "./images/icon-arrow-down.svg";
         })
     }
 }, false)
@@ -25,10 +29,10 @@ document.addEventListener("click", (e) => {
 openMenu.addEventListener("click", (e) => {
     listContainer.classList.add("active")
     main.classList.add("active")
-})
+}, false)
 
 closeMenu.addEventListener("click", () => {
     listContainer.classList.remove("active")
     main.classList.remove("active")
-})
+}, false)
 
